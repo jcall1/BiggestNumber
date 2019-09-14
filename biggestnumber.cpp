d200b0489a3a675ad18a584314fca2d35d1a9ff9
jcall1 <jcall1@mail.csuchico.edu>
2019-09-13 20:39:27 -0700

    finding the biggest of two numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,6 +6,7 @@ int main()
   //declare variables here
   int number1;
   int number2;
+  int biggest;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
@@ -15,11 +16,15 @@ int main()
   //get second input using std::cin>> again with a second variable
   std::cin>>number2;
   //determine if first or second is bigger and store that in a third variable
-
+  if (number1 > number2)
+  biggest = number1;
+  else
+  biggest = number2;
+ 
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
-
+  std::cout<<biggest;
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
